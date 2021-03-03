da9772ffe572b2c3a6490753425d55c7fb75526b
hannebel <hannebel@stanford.edu>
2021-03-02 19:12:03 -0800

    removed extra code for 3 motor control

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,13 +2,9 @@
 #include <TMCStepper_UTILITY.h>
 #include <AccelStepper.h>
 
-static inline int sgn(int val) {
- if (val < 0) return -1;
- if (val==0) return 0;
- return 1;
-}
 
-// byte[0]: which motor to move: 0 x, 1 y, 2 z, 3 LED, 4 Laser
+
+// byte[0]: what is controlled: 2 z, 
 // byte[1]: what direction: 1 forward, 0 backward
 // byte[2]: how many micro steps - upper 8 bits
 // byte[3]: how many micro steps - lower 8 bits
@@ -42,46 +38,19 @@ static const int UART_CS_S1 = 47;
 #define STEPPER_SERIAL Serial3
 static const uint8_t X_driver_ADDRESS = 0b00;
 static const float R_SENSE = 0.11f;
-TMC2209Stepper X_driver(&STEPPER_SERIAL, R_SENSE, X_driver_ADDRESS);
-TMC2209Stepper Y_driver(&STEPPER_SERIAL, R_SENSE, X_driver_ADDRESS);
+//TMC2209Stepper X_driver(&STEPPER_SERIAL, R_SENSE, X_driver_ADDRESS);
+//TMC2209Stepper Y_driver(&STEPPER_SERIAL, R_SENSE, X_driver_ADDRESS);
 TMC2209Stepper Z_driver(&STEPPER_SERIAL, R_SENSE, X_driver_ADDRESS);
-
-// driver 1 actuator 1
-static const int X_dir = 28;
-static const int X_step = 26;
-static const int X_en = 36;
-static const int X_N_microstepping = 2;
-static const long steps_per_mm_X = 78.74*X_N_microstepping; 
-constexpr float MAX_VELOCITY_X_mm = 7.62; 
-constexpr float MAX_ACCELERATION_X_mm = 100;
-
-// driver 2 - PL25
-//static const long steps_per_mm_XY = 30*4; 
-//constexpr float MAX_VELOCITY_Y_mm = 25; 
-//constexpr float MAX_ACCELERATION_Y_mm = 500; // 50 ms to reach 15 mm/s
-static const int Y_dir = 24;
-static const int Y_step = 22;
-static const int Y_en = 36;
-static const int Y_N_microstepping = 2;
-static const long steps_per_mm_Y = 78.74*Y_N_microstepping; 
-constexpr float MAX_VELOCITY_Y_mm = 7.62; 
-constexpr float MAX_ACCELERATION_Y_mm = 100;
 
 static const int Z_dir = 23;
 static const int Z_step = 25;
 static const int Z_N_microstepping = 2;
-static const long steps_per_mm_Z = 82.02*Y_N_microstepping; 
+static const long steps_per_mm_Z = 82.02*Z_N_microstepping; 
 constexpr float MAX_VELOCITY_Z_mm = 18.29; 
 constexpr float MAX_ACCELERATION_Z_mm = 100;
 
-AccelStepper stepper_X = AccelStepper(AccelStepper::DRIVER, X_step, X_dir);
-AccelStepper stepper_Y = AccelStepper(AccelStepper::DRIVER, Y_step, Y_dir);
 AccelStepper stepper_Z = AccelStepper(AccelStepper::DRIVER, Z_step, Z_dir);
 
-long X_commanded_target_position = 0;
-bool X_commanded_movement_in_progress = false;
-long Y_commanded_target_position = 0;
-bool Y_commanded_movement_in_progress = false;
 long Z_commanded_target_position = 0;
 bool Z_commanded_movement_in_progress = false;
 
@@ -93,10 +62,10 @@ static const float ACCELERATION_MAX = 500; // mm/s/s
 static const int FULLSTEPS_PER_MM = 40;
 
 // limit switches for Z axis
-const byte switch_top=30; //Mel
-const byte switch_bottom=32; //Mel
-volatile bool flag_disable_motorUp = false; //Mel
-volatile bool flag_disable_motorDown = false;//Mel
+const byte switch_top=32; 
+const byte switch_bottom=30; 
+volatile bool flag_disable_motorUp = false; 
+volatile bool flag_disable_motorDown = false;
 
 
 
@@ -109,12 +78,7 @@ void setup() {
 
   pinMode(13, OUTPUT);
   digitalWrite(13,LOW);
-    
-  pinMode(X_dir, OUTPUT);
-  pinMode(X_step, OUTPUT);
 
-  pinMode(Y_dir, OUTPUT);
-  pinMode(Y_step, OUTPUT);
 
   pinMode(Z_dir, OUTPUT);
   pinMode(Z_step, OUTPUT);
@@ -122,45 +86,16 @@ void setup() {
   pinMode(UART_CS_S0, OUTPUT);
   pinMode(UART_CS_S1, OUTPUT);
 
-  pinMode(switch_top,INPUT_PULLUP); //Mel
-  pinMode(switch_bottom,INPUT_PULLUP); //Mel
-  attachInterrupt(digitalPinToInterrupt(switch_top),interrupt_motorUp,RISING); //Mel
-  attachInterrupt(digitalPinToInterrupt(switch_bottom),interrupt_motorDown,RISING); //Mel
+  pinMode(switch_top,INPUT_PULLUP); 
+  pinMode(switch_bottom,INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(switch_top),interrupt_motorUp,RISING); 
+  attachInterrupt(digitalPinToInterrupt(switch_bottom),interrupt_motorDown,RISING); 
 
   // initialize stepper driver
   STEPPER_SERIAL.begin(115200);
 
-  select_driver(1);
-  while(!STEPPER_SERIAL);
-  X_driver.begin();
-  X_driver.I_scale_analog(false);  
-  X_driver.rms_current(250,0.2); //I_run and holdMultiplier
-  X_driver.microsteps(X_N_microstepping);
-  X_driver.pwm_autoscale(true);
-  X_driver.TPOWERDOWN(2);
-  X_driver.en_spreadCycle(false);
-  X_driver.toff(4);
-  stepper_X.setPinsInverted(false, false, true);
-  stepper_X.setMaxSpeed(MAX_VELOCITY_X_mm*steps_per_mm_X);
-  stepper_X.setAcceleration(MAX_ACCELERATION_X_mm*steps_per_mm_X);
-  stepper_X.enableOutputs();
-  
-  select_driver(2);
-  while(!STEPPER_SERIAL);
-  Y_driver.begin();
-  Y_driver.I_scale_analog(false);  
-  Y_driver.rms_current(250,0.2); //I_run and holdMultiplier
-  Y_driver.microsteps(Y_N_microstepping);
-  Y_driver.pwm_autoscale(true);
-  Y_driver.TPOWERDOWN(2);
-  Y_driver.en_spreadCycle(false);
-  Y_driver.toff(4);
-  stepper_Y.setPinsInverted(false, false, true);
-  stepper_Y.setMaxSpeed(MAX_VELOCITY_Y_mm*steps_per_mm_Y);
-  stepper_Y.setAcceleration(MAX_ACCELERATION_Y_mm*steps_per_mm_Y);
-  stepper_Y.enableOutputs();
-
-  select_driver(3);
+  digitalWrite(UART_CS_S0, LOW);
+  digitalWrite(UART_CS_S1, HIGH);
   while(!STEPPER_SERIAL);
   Z_driver.begin();
   Z_driver.I_scale_analog(false);  
@@ -192,48 +127,7 @@ void loop() {
     buffer_rx_ptr = buffer_rx_ptr + 1;
     if (buffer_rx_ptr == CMD_LENGTH) {
       buffer_rx_ptr = 0;
-      if(buffer_rx[0]==0)
-      {
-        long relative_position = long(buffer_rx[1]*2-1)*(long(buffer_rx[2])*256 + long(buffer_rx[3]));
-        int n_microstepping = buffer_rx[4];
-        float steps_per_mm = FULLSTEPS_PER_MM*n_microstepping;
-        float velocity = VELOCITY_MAX*(float(long(buffer_rx[5])*256 + long(buffer_rx[6]))/65535);
-        float acceleration = ACCELERATION_MAX*(float(long(buffer_rx[7])*256 + long(buffer_rx[8]))/65535);
-        select_driver(1);
-        while(!STEPPER_SERIAL);
-        X_driver.begin();
-        if(n_microstepping==1)
-          X_driver.microsteps(0);
-        else
-          X_driver.microsteps(n_microstepping);
-        stepper_X.setMaxSpeed(velocity*steps_per_mm);
-        stepper_X.setAcceleration(acceleration*steps_per_mm);
-        
-        X_commanded_target_position = (stepper_X.currentPosition()+relative_position);
-        stepper_X.moveTo(X_commanded_target_position);
-        X_commanded_movement_in_progress = true;
-      }
-      if(buffer_rx[0]==1)
-      {
-        long relative_position = long(buffer_rx[1]*2-1)*(long(buffer_rx[2])*256 + long(buffer_rx[3]));
-        int n_microstepping = buffer_rx[4];
-        float steps_per_mm = FULLSTEPS_PER_MM*n_microstepping;
-        float velocity = VELOCITY_MAX*(float(long(buffer_rx[5])*256 + long(buffer_rx[6]))/65535);
-        float acceleration = ACCELERATION_MAX*(float(long(buffer_rx[7])*256 + long(buffer_rx[8]))/65535);
-        select_driver(2);
-        while(!STEPPER_SERIAL);
-        Y_driver.begin();
-        if(n_microstepping==1)
-          Y_driver.microsteps(0);
-        else
-          Y_driver.microsteps(n_microstepping);
-        stepper_Y.setMaxSpeed(velocity*steps_per_mm);
-        stepper_Y.setAcceleration(acceleration*steps_per_mm);
-        
-        Y_commanded_target_position = (stepper_Y.currentPosition()+relative_position);
-        stepper_Y.moveTo(Y_commanded_target_position);
-        Y_commanded_movement_in_progress = true;
-      }
+
       if(buffer_rx[0]==2)
       {
         long relative_position = long(buffer_rx[1]*2-1)*(long(buffer_rx[2])*256 + long(buffer_rx[3]));
@@ -241,7 +135,7 @@ void loop() {
         float steps_per_mm = 100*n_microstepping;
         float velocity = VELOCITY_MAX*(float(long(buffer_rx[5])*256 + long(buffer_rx[6]))/65535);
         float acceleration = ACCELERATION_MAX*(float(long(buffer_rx[7])*256 + long(buffer_rx[8]))/65535);
-        select_driver(3);
+    //    select_driver(3);
         while(!STEPPER_SERIAL);
         Z_driver.begin();
         if(n_microstepping==1)
@@ -255,67 +149,8 @@ void loop() {
         stepper_Z.moveTo(Z_commanded_target_position);
         Z_commanded_movement_in_progress = true;
       }      
-      //break; // exit the while loop after reading one message
-      if(buffer_rx[0]==4)
-      {
-        long relative_position = long(buffer_rx[1]*2-1)*(long(buffer_rx[2])*256 + long(buffer_rx[3]));
-        int n_microstepping = buffer_rx[4];
-        float steps_per_mm = FULLSTEPS_PER_MM*n_microstepping;
-        float velocity = VELOCITY_MAX*(float(long(buffer_rx[5])*256 + long(buffer_rx[6]))/65535);
-        float acceleration = ACCELERATION_MAX*(float(long(buffer_rx[7])*256 + long(buffer_rx[8]))/65535);
-        select_driver(1);
-        while(!STEPPER_SERIAL);
-        X_driver.begin();
-        if(n_microstepping==1)
-          X_driver.microsteps(0);
-        else
-          X_driver.microsteps(n_microstepping);
-        stepper_X.setMaxSpeed(velocity*steps_per_mm);
-        stepper_X.setAcceleration(acceleration*steps_per_mm);
 
-        long start_position = stepper_X.currentPosition();
-        long target_position = (stepper_X.currentPosition()+relative_position);
-        
-        // X_commanded_target_position = (stepper_X.currentPosition()+relative_position);
-        // stepper_X.moveTo(X_commanded_target_position);
-        // X_commanded_movement_in_progress = true;
 
-        for(int i=0;i<3;i++)
-        {
-          stepper_X.runToNewPosition(target_position);
-          stepper_X.runToNewPosition(start_position);
-        }
-      }
-      if(buffer_rx[0]==6)
-      {
-        long relative_position = long(buffer_rx[1]*2-1)*(long(buffer_rx[2])*256 + long(buffer_rx[3]));
-        int n_microstepping = buffer_rx[4];
-        float steps_per_mm = 100*n_microstepping;
-        float velocity = VELOCITY_MAX*(float(long(buffer_rx[5])*256 + long(buffer_rx[6]))/65535);
-        float acceleration = ACCELERATION_MAX*(float(long(buffer_rx[7])*256 + long(buffer_rx[8]))/65535);
-        select_driver(3);
-        while(!STEPPER_SERIAL);
-        Z_driver.begin();
-        if(n_microstepping==1)
-          Z_driver.microsteps(0);
-        else
-          Z_driver.microsteps(n_microstepping);
-        stepper_Z.setMaxSpeed(velocity*steps_per_mm);
-        stepper_Z.setAcceleration(acceleration*steps_per_mm);
-
-        long start_position = stepper_Z.currentPosition();
-        long target_position = (stepper_Z.currentPosition()+relative_position);
-        
-        // X_commanded_target_position = (stepper_X.currentPosition()+relative_position);
-        // stepper_X.moveTo(X_commanded_target_position);
-        // X_commanded_movement_in_progress = true;
-
-        for(int i=0;i<3;i++)
-        {
-          stepper_Z.runToNewPosition(target_position);
-          stepper_Z.runToNewPosition(start_position);
-        }
-      }
       if(buffer_rx[0]==7)
         analogWrite(heater_1_pwm,buffer_rx[1]);
       if(buffer_rx[0]==8)
@@ -323,19 +158,6 @@ void loop() {
     }
   }
 
-
-  // check if commanded position has been reached
-  if(X_commanded_movement_in_progress && stepper_X.currentPosition()==X_commanded_target_position)
-    X_commanded_movement_in_progress = false;
-  // move motors
-  if(X_commanded_movement_in_progress)
-    stepper_X.run();
-  
-  if(Y_commanded_movement_in_progress && stepper_Y.currentPosition()==Y_commanded_target_position)
-    Y_commanded_movement_in_progress = false;
-  // move motors
-  if(Y_commanded_movement_in_progress)
-    stepper_Y.run();
 
   if(Z_commanded_movement_in_progress && stepper_Z.currentPosition()==Z_commanded_target_position)
     Z_commanded_movement_in_progress = false;
@@ -347,7 +169,7 @@ void loop() {
     }
     if ((flag_disable_motorUp==false)&&(stepper_Z.distanceToGo()<0)) { //Mel
       stepper_Z.run();
-      flag_disable_motorUp=false;
+      flag_disable_motorDown=false;
     }
   }
 
@@ -423,33 +245,20 @@ void interrupt_motorDown(){
 
     flag_disable_motorDown=true;
 }
-// utils
+
+/***************************************************************************************************/
+/********************************************* UTILS **********************************************/
+/***************************************************************************************************/
+
 long signed2NBytesUnsigned(long signedLong,int N)
 {
   long NBytesUnsigned = signedLong + pow(256L,N)/2;
   //long NBytesUnsigned = signedLong + 8388608L;
   return NBytesUnsigned;
 }
-void select_driver(int id)
-{
-  if(id==1)
-  {
-    digitalWrite(UART_CS_S0, LOW);
-    digitalWrite(UART_CS_S1, LOW);
-  }
-  if(id==2)
-  {
-    digitalWrite(UART_CS_S0, HIGH);
-    digitalWrite(UART_CS_S1, LOW);
-  }
-  if(id==3)
-  {
-    digitalWrite(UART_CS_S0, LOW);
-    digitalWrite(UART_CS_S1, HIGH);
-  }
-  if(id==4)
-  {
-    digitalWrite(UART_CS_S0, HIGH);
-    digitalWrite(UART_CS_S1, HIGH);
-  }
+
+static inline int sgn(int val) {
+ if (val < 0) return -1;
+ if (val==0) return 0;
+ return 1;
 }
